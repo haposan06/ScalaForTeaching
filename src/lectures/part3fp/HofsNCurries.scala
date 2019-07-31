961a485308d2de8a216c7546c8912d8bcb85593e
Napitupulu <venj81@sg.uob>
2019-07-31 20:43:17 +0700

    Training materials: Curried function

--- src/lectures/part3fp/HofsNCurries.scala
+++ src/lectures/part3fp/HofsNCurries.scala
@@ -14,5 +14,27 @@ object HofsNCurries extends App {
   }
 
   val result = nTimes(x => x + 1 , 2, 1)
-  println(result)
+
+  def nTimesBetter (f: Int=> Int, n:Int): Int=>Int = {
+    if (n <= 0 ) x=> x
+    else
+      x=> nTimesBetter(f, n-1)(f(x))
+  }
+
+  val supperAdder: Int => Int => Int = x=> {
+        println("X=> " + x)
+    y => {
+      println("Y=> " + y)
+      y + x
+    }
+  }
+      val res= supperAdder(1)(3)
+  println(res)
+      val plus10 = nTimesBetter(x=> x+1, 10)
+
+      def curriedFormatter(c:String)(d:Double):String = c.format(d)
+      val standardFormat = curriedFormatter("%4.2f")
+      val resString = standardFormat(3)
+
+
 }
